f351f175800ca9ad01aeb4532283fa486483e8fb
JUNYA\junya <zyn19941124@gmail.com>
2026-06-21 18:06:22 +0900

    ファイル移動

--- chapter02/index.js
+++ chapter02/index.js
@@ -1,1 +1,23 @@
-console.log('hello Node.js')
+function isGlobal(){
+    console.log(this === global)
+}
+
+isGlobal()
+
+function printName(){
+    setTimeout(function(){
+        console.log(this.name)
+    }.bind(this),1000)
+
+    setTimeout(()=>{
+        console.log(this.name)
+    },1000)
+    
+}
+
+const obj = {
+    name: 'obj-name',
+    printName:printName
+}
+
+obj.printName()
